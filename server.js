5f4e885e191653d3f63b8b33d8071905879b1a01
rudzkimichal <michal.rudzki@yahoo.com>
2021-03-10 02:43:01 +0100

    changes to server.js

--- server.js
+++ server.js
@@ -1,19 +1,6 @@
 const express = require('express');
-
 const app = express();
-
-
-
-
-
-
-
-
-
-
-
 const port = 8080;
+require('./app/routes')(app);
 
-app.listen(
-  port, () => { console.log('Server running on port ' + port) }
-);
+app.listen(port, () => {console.log(`Server running on port ${port}`)});
